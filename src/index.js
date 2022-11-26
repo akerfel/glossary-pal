62fdbb74a47a61423c2df849a70b236cfbf12481
carlenge <carl.engelhardt@gmail.com>
2022-11-26 23:51:50 +0100

    hello world template

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import {render, h} from "vue";
-
-window.React = {createElement:h};
-
-const VueRoot = require("/src/presenters/VueRoot.js").default;
-
-render( 
-    <VueRoot/>, document.getElementById('root')
-);
