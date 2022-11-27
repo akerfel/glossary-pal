be82c577d59ac5b449c8cc174d03e13d7aefa4af
Magnus Åkerfeldt <akerfel@kth.se>
2022-11-27 16:01:47 +0100

    Removed some margin

--- src/App.vue
+++ src/App.vue
@@ -27,7 +27,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
   background-color: rgb(227, 107, 107);
 }
 </style>
