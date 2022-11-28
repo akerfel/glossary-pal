63f5ebfba91e43eeeebf82e82ce179e578f72d22
Magnus Åkerfeldt <akerfel@kth.se>
2022-11-28 23:07:03 +0100

    Small router refactor

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
+import HomePresenter from "../presenters/HomePresenter.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -6,7 +7,7 @@ const router = createRouter({
     {
       path: "/",
       name: "home",
-      component: () => import("../presenters/HomePresenter.vue"),
+      component: () => HomePresenter,
     },
   ],
 });
