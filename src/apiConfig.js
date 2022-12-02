dff8abe8bebdcca91af3cd315210088decca5c05
carlenge <carl.engelhardt@gmail.com>
2022-12-02 22:30:49 +0100

    delete apiConfig.js

--- src/apiConfig.js
+++
@@ -1,4 +0,0 @@
-const BASE_URL_TRANSLATE="https://google-translate1.p.rapidapi.com/language/translate/v2/languages";
-const API_KEY_TRANSLATE="a08473903dmsh66630bf35a229b6p15706ajsn80e79f4f005b";
-
-export {BASE_URL_TRANSLATE, API_KEY_TRANSLATE}
