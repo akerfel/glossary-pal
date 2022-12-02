9276efa32cb2df0798d5c26c82cf84778443de78
carlenge <carl.engelhardt@gmail.com>
2022-12-02 16:43:13 +0100

    actually add postcreatedeckview

---
+++ src/views/PostCreateDeckView.vue
@@ -0,0 +1,161 @@
+<script setup>
+import {
+  NInput,
+  NInputGroup,
+  NSelect,
+  NButton,
+  NScrollbar,
+  NAlert,
+} from "naive-ui";
+</script>
+
+<script>
+export default {
+  props: ["deckCreation", "onGoToHome", "onCreateAnotherDeck"],
+
+  methods: {
+    clickedGoToHomeACB() {
+      this.onGoToHome();
+    },
+    clickedCreateAnotherDeckACB() {
+      this.onCreateAnotherDeck();
+    },
+  },
+};
+</script>
+
+<template>
+  <div v-if="this.deckCreation.creationSuccessfull" class="postcreateview">
+    <n-alert class="alert" title="Deck created" type="success">
+      Your deck "{{ this.deckCreation.deckTitle }}" has been successfully
+      created.
+    </n-alert>
+    <div class="deckWords">
+      <h2 id="deckWordsTitle">Words in deck</h2>
+      <div id="scrollbarDiv">
+        <div id="deckWordsColumns">
+          <span class="fromLangTextAddWord">{{
+            this.deckCreation.fromLang
+          }}</span>
+          <span>{{ this.deckCreation.toLang }}</span>
+        </div>
+        <n-scrollbar style="max-height: 500px">
+          <div id="scrollbarWord" v-for="word in this.deckCreation.deckWords">
+            <span class="fromLangTextAddWord">{{ word.from }}</span>
+            <span>{{ word.to }}</span>
+          </div>
+        </n-scrollbar>
+      </div>
+    </div>
+    <div>
+      <n-button type="primary" @click="clickedGoToHomeACB"
+        >Go back to home</n-button
+      >
+      <n-button
+        style="margin-left: 30px"
+        type="primary"
+        @click="clickedCreateAnotherDeckACB"
+        >Create another deck</n-button
+      >
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.postcreateview {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 440px;
+}
+
+.title {
+  color: rgb(0, 194, 81);
+  margin-bottom: 15px;
+}
+
+#toLangText {
+  margin-left: 125px;
+}
+
+.deckparams {
+  width: 100%;
+  margin-bottom: 50px;
+}
+
+.deckname {
+  margin-bottom: 15px;
+}
+
+#deckLangFrom {
+  width: 40%;
+}
+
+#deckLangTo {
+  width: 40%;
+  float: right;
+}
+
+.addword {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+#addWordTitle {
+  color: rgb(0, 194, 81);
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+.fromLangTextAddWord {
+  display: inline-block;
+  width: 42%;
+}
+
+.deckWords {
+  width: 100%;
+  margin-bottom: 25px;
+}
+
+#deckWordsTitle {
+  color: rgb(0, 194, 81);
+  text-align: center;
+  margin-bottom: 10px;
+}
+
+#deckWordsColumns {
+  background-color: #181818;
+  color: rgb(0, 194, 81);
+  height: 28px;
+  margin-bottom: 5px;
+  padding-left: 5px;
+}
+
+#scrollbarDiv {
+  border: 3px solid rgb(206, 50, 50);
+  background-color: rgb(255, 255, 255);
+}
+
+#scrollbarWord {
+  background-color: rgb(255, 255, 255);
+  color: rgb(0, 0, 0);
+  width: 95.8%;
+  margin-left: 5px;
+  margin-right: 5px;
+  margin-bottom: 5px;
+}
+
+#removeWordButton {
+  width: 48px;
+  height: 24px;
+  float: right;
+}
+
+.alert {
+  margin-bottom: 25px;
+}
+
+#createdeck {
+  width: 100%;
+}
+</style>
