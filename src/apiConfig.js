30500b0fbe53c849f5cb1a48ecb14a6a8025cd96
Shotaro Ishii <shotaro@kth.se>
2022-12-05 19:21:56 +0100

    Delete apiConfig

--- src/apiConfig.js
+++
@@ -1,8 +0,0 @@
-const BASE_URL_GET_LANG =
-  "https://google-translate1.p.rapidapi.com/language/translate/v2/languages";
-const BASE_URL_TRANSLATE =
-  "https://google-translate1.p.rapidapi.com/language/translate/v2";
-//const API_KEY_GOOGLE = "a08473903dmsh66630bf35a229b6p15706ajsn80e79f4f005b";
-const API_KEY_GOOGLE = "07a340db45msh629a8e25f6c4d25p186330jsna11314365a78"
-
-export { BASE_URL_GET_LANG, BASE_URL_TRANSLATE, API_KEY_GOOGLE };
