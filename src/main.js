9e9852b605c3dc17334ab1a963362d555a9d092a
carlenge <carl.engelhardt@gmail.com>
2022-12-19 06:55:17 +0100

    comment on firebase auth persistance being NONE

--- src/main.js
+++ src/main.js
@@ -7,10 +7,16 @@ import "firebase/auth";
 import firebaseConfig from "/src/firebaseConfig.js";
 
 firebase.initializeApp(firebaseConfig);
-// Get the FirebaseAuth instance
-var auth = firebase.auth();
 
-// Set the persistence to 'local'
+// Set persistence to NONE, i.e. user will be logged out upon refreshing page.
+// We do this because knowing whether user is logged in or not upon refresh
+// seems difficult with firebase auth (the getter is not a Promise, but you have  
+// to wait an unknown time), and we do not have any persistent local storage(?).
+// Also, page refresh in general is wonky due to this being a SPA (only on deployed version,
+// refreshing works well when running locally), so not logging the user out on refresh
+// would not add much to the application.
+
+var auth = firebase.auth();
 auth
   .setPersistence(firebase.auth.Auth.Persistence.NONE)
   .catch(function (error) {
